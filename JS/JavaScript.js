f03ca00d23b43327f963f4df3b298f80abee52dc
Warbi2601 <joshwarbi@hotmail.com>
2019-06-12 01:44:52 +0100

    Added functionality for the new banner with scrolling feature

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -22,3 +22,15 @@ function closeNav() {
 $(document).ready(function() {
 	$('.lastUpdated').text("Page Last Updated: " + new Date(document.lastModified).toLocaleDateString("en-GB"));
 });
+
+var handleScroll = function() {
+	// Get the height of the hero section
+	var heroHeight = document.querySelector('#hero').offsetHeight;
+	var diagonal = document.querySelector('.hero-diagonal polygon');
+	// Calculate the skew value
+	var skew = (window.scrollY / heroHeight) * 100;
+	// Assign the skew value to the polygon points attr
+	// The value needs to be set as an array
+	diagonal.setAttribute('points', [0,100, 100, skew, 100,100]);
+  }
+  window.addEventListener("scroll", handleScroll);
